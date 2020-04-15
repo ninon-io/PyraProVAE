83aa8b7fa6f7b8bb2c0cb13f94c0e7a78328666c
Ninon Devis <ninon.devis@gmail.com>
2020-04-15 20:15:03 +0200

    Finalize Model

--- vae.py
+++ vae.py
@@ -1,27 +1,58 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
+import random
 
 
-class BaseEncoder(nn.Module):
+class VaeModel(nn.Module):
+    def __init__(self, encoder, decoder, teacher_forcing=True):
+        super(VaeModel, self).__init__()
+        self.tf = teacher_forcing
+        self.encoder = encoder
+        self.decoder = decoder
+        self.hidden_to_mu = nn.Linear(2 * encoder.hidden_size, encoder.latent_size)
+        self.hidden_to_sigma = nn.Linear(2 * encoder.hidden_size, encoder.latent_size)
+
+    def forward(self, x):
+        # Encoder pass
+        batch_size = x.size[0]
+        h_enc, c_enc = self.encoder.init_hidden(batch_size)
+        hidden = self.encoder(x, h_enc, c_enc)
+        # Reparameterization
+        mu = self.hidden_to_mu(hidden)
+        sigma = self.hidden_to_sigma(hidden)
+        eps = torch.randn_like(mu).detach().to(x.device)
+        latent = (sigma.exp().sqrt() * eps) + mu
+
+        # Decoder pass
+        x_reconstruct = self.decoder(latent, x, teacher_forcing=False)
+
+        return mu, sigma, latent, x_reconstruct
+
+    def generate(self, latent):
+        # Create dumb target
+        input_shape = (1, self.decoder.seq_length, self.decoder.input_size)
+        trg = torch.zeros(input_shape)
+        # Forward pass in the decoder
+        generated_bar = self.decoder(latent.unsqueeze(0), trg, teacher_forcing=False)
+
+        return generated_bar
+
+
+class HierarchicalEncoder(nn.Module):
     def __init__(self, input_dim, hidden_size, latent_size, batch_size, num_layers=2):
-        super(BaseEncoder, self).__init__()
-        self.input_dim = input_dim
+        super(HierarchicalEncoder, self).__init__()
         self.hidden_size = hidden_size
         self.latent_size = latent_size
-        self.batch_size = batch_size
         self.num_layers = num_layers
 
         # Define the LSTM layer
-        self.LSTM = nn.LSTM(self.input_dim, self.hidden_size, self.num_layers, bidirectional=True)
+        self.RNN = nn.LSTM(self.input_dim, self.hidden_size, batch_first=True, num_layers=num_layers, bidirectional=True, dropout=0.6)
 
-        # Define output layer but maybe not
-        self.linear = nn.Linear(self.hidden_size, latent_size)
-
-    def init_hidden(self):
-        # initialize the the hidden state
-        return (torch.zero(self.num_layers, self.batch_size, self.hidden_size),
-                torch.zero(self.num_layers, self.batch_size, self.hidden_size))
+    def init_hidden(self, batch_size=1):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # initialize the the hidden state // Bidirectionnal so num_layers * 2 \\
+        return (torch.zero(self.num_layers * 2, batch_size, self.hidden_size, dtype=torch.float, device=device),
+                torch.zero(self.num_layers * 2, batch_size, self.hidden_size, dtype=torch.float, device=device))
 
     def forward(self, x, h0, c0):
         batch_size = x.shape[0]
@@ -32,16 +63,68 @@ class BaseEncoder(nn.Module):
         return h
 
 
-class BaseDecoder(nn.Module):
-    def __init__(self, input_size, latent_size, cond_hidden_size, cond_outdim):
+class HierarchicalDecoder(nn.Module):
+    def __init__(self, input_size, latent_size, cond_hidden_size, cond_outdim, dec_hidden_size, num_layers, num_subsequences, seq_length):
+        super(HierarchicalDecoder, self).__init__()
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        self.fc_init_cond = nn.Linear(latent_size, cond_hidden_size*num_layers)
+        self.conductor_RNN = nn.LSTM(latent_size//num_subsequences, cond_hidden_size, batch_first=True, num_layers=2, bidirectional=False, dropout=0.6)
+        self.conductor_output = nn.Linear(cond_hidden_size, cond_outdim)
+        self.fc_init_dec = nn.Linear(cond_outdim, dec_hidden_size*num_layers)
+        self.decoder_RNN = nn.LSTM(cond_outdim + input_size, dec_hidden_size, batch_first=True, num_layers=2, bidirectional=False, dropout=0.6)
+        self.decoder_output = nn.Linear(dec_hidden_size, input_size)
+
+        self.num_subsequences = num_subsequences
+        self.input_size = input_size
+        self.cond_hidden_size = cond_hidden_size
+        self.dec_hidden_size = dec_hidden_size
+        self.num_layers = num_layers
+        self.seq_length = seq_length
+        self.teacher_forcing_ratio = 0.5
+
+    def forward(self, latent, target, teacher_forcing):
+        batch_size = latent.shape[0]
+        subseq_size = self.seq_length // self.num_subsequences
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Get the initial states of the conductor
+        h0_cond = self.tanh(self.fc_init_cond(latent)).view(self.num_layers, batch_size, -1).contiguous()
+        # Divide the latent space into subsequences:
+        latent = latent.view(batch_size, self.num_subsequences, -1)
+        # Pass through the conductor
+        subseq_embeddings, _ = self.conductor_RNN(latent, (h0_cond, h0_cond))
+        subseq_embeddings = self.conductor_output(subseq_embeddings)
+
+        # Get the initial state of decoder
+        h0s_dec = self.tanh(self.fc_init_dec(subseq_embeddings)).view(self.num_layers, batch_size, -1).contiguous()
+
+        # Init the output seq and the first token to 0 tensors
+        out = torch.zeros(batch_size, self.seq_length, self.input_size, dtype=torch.float, device=device)
+        token = torch.zeros(batch_size, subseq_size, self.input_size, dtype=torch.float, device=device)
+
+        # autoregressively output token
+        for subseq in range(self.num_subsequences):
+            subseq_embedding = subseq_embeddings[:, subseq, :]
+            h0_dec = h0s_dec[:, subseq, :].contiguous()
+            c0_dec = h0s_dec[:, :, subseq, :].contiguous()
+            # Concat the previous token and the current subseq embedding as input
+            dec_input = torch.cat((token, subseq_embedding.unsqueeze(1).expand(-1, subseq_size, -1)), -1)
+            # Pass through the decoder
+            token, (h0_dec, c0_dec) = self.decoder_RNN(dec_input, (h0s_dec, c0_dec))
+            token = self.decoder_output(token)
+            # Fill the out tensor with the token
+            token = out[:, subseq*subseq_size:((subseq + 1) * subseq_size), :]
+            # If teacher forcing replace the output token by the real one sometimes
+            if teacher_forcing:
+                if random.random() <= self.teacher_forcing_ratio:
+                    token = target[:, subseq*subseq_size: ((subseq + 1) * subseq_size), :]
+        return out
 
 
 
 
-class VaeModel(nn.Module):
-    def __init__(self, batch_size, channel):
-        super(VaeModel, self).__init__()
-        self.batch_size = batch_size
-        self.channel = channel
+
+
 
 
