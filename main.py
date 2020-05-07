fed0080d730204465606a402889f7069866e5f4b
Ninon Devis <ninon.devis@gmail.com>
2020-05-07 22:07:21 +0200

    Main function for tensorboard [not working] and save model

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import argparse
 import torch.nn.utils
 from guppy import hpy
 
+from tensorboardX import SummaryWriter
+
 from learn import Learn
 import data_loader
-import dumb_dataloader
-import vae
 
 h = hpy()
+
 data_dir = 'midi_short_dataset'
 # midi_path = "fast-1/ninon/datasets/maestro_folder/train"
 # test_path = "fast-1/ninon/datasets/maestro_folder/test"
+
+# Tensorboard initialization
+writer = SummaryWriter('./output/runs')
 
 # get the arguments, if not on command line, the arguments are default
 parser = argparse.ArgumentParser(description='Music VAE')
@@ -21,7 +25,7 @@ parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--input_dim', type=int, default=100,
                     help='correspond to the out_channels of the conv (defaults: 123)')
-parser.add_argument('--epochs', type=int, default=14, metavar='N',
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 14)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -48,23 +52,24 @@ if __name__ == "__main__":
                                                                                  frame_bar=100,
                                                                                  batch_size=args.batch_size,
                                                                                  export=False)
-    # Here Dumb Dataloader
-    # train_loader, test_loader, train_set, test_set = dumb_dataloader.get_data_loader(bar_dir=data_dir, frame_bar=100,
-    #                                                                                  batch_size=args.batch_size,
-    #                                                                                  export=False)
 
     learn = Learn(train_loader=train_loader, test_loader=test_loader, train_set=train_set, test_set=test_set,
                   batch_size=args.batch_size, seed=args.seed, lr=args.lr)
 
     # Set time
-    # print(h.heap())
     time0 = time()
     # Initial training of the model
+    # learn.save('/slow-1/ninon/output/models/weights/', 'slow-1/ninon/output/models/entire_model/', epoch=0)
+    learn.save('./models/weights/', './models/entire_model/', epoch=0)
     learn.test()  # First test on randomly initialized data
-    for epoch in range(args.epochs):
-        print('epoch:' + str(epoch))
+    for epoch in range(1, args.epochs + 1, 1):
+        print('Epoch:' + str(epoch))
         learn.train()
         learn.test()
+        learn.fill_tensorboard(epoch)
+        # learn.save('/slow-1/ninon/output/models/weights/', 'slow-1/ninon/output/models/entire_model/', epoch)
+        learn.save('./models/weights/', './models/entire_model/', epoch)
+    writer.close()
     # learn.plot()
 
     print('\nTraining Time in minutes =', (time() - time0) / 60)
