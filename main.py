7d7eea27175ea35e13fb1cf904ae7319f511e5a1
Ninon Devis <ninon.devis@gmail.com>
2020-05-19 23:15:57 +0200

    Clean Unused Lines

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import argparse
 import torch.nn.utils
 from guppy import hpy
 
-from tensorboardX import SummaryWriter
-
 from learn import Learn
 import data_loader
-from comparison import Comparison
 
 h = hpy()
 
@@ -15,8 +12,6 @@ data_dir = 'midi_short_dataset'
 # midi_path = "fast-1/ninon/datasets/maestro_folder/train"
 # test_path = "fast-1/ninon/datasets/maestro_folder/test"
 
-# Tensorboard initialization
-# writer = SummaryWriter('./output/runs')
 
 # get the arguments, if not on command line, the arguments are default
 parser = argparse.ArgumentParser(description='Music VAE')
@@ -26,7 +21,7 @@ parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--input_dim', type=int, default=100,
                     help='correspond to the out_channels of the conv (defaults: 123)')
-parser.add_argument('--epochs', type=int, default=1, metavar='N',
+parser.add_argument('--epochs', type=int, default=2, metavar='N',
                     help='number of epochs to train (default: 14)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -61,18 +56,16 @@ if __name__ == "__main__":
     time0 = time()
     # Initial training of the model
     # learn.save('/slow-1/ninon/output/models/weights/', 'slow-1/ninon/output/models/entire_model/', epoch=0)
-    learn.save('./models/weights/', './models/entire_model/', epoch=0)
-    # learn.test(epoch=0)  # First test on randomly initialized data
+    # learn.save('./models/weights/', './models/entire_model/', epoch=0)
+    learn.test(epoch=0)  # First test on randomly initialized data
     for epoch in range(1, args.epochs + 1, 1):
         print('Epoch:' + str(epoch))
-        learn.test_random(data_set, nb_test=1)
-        learn.train(epoch)
-        learn.test(epoch)
-        # learn.fill_tensorboard(epoch)
+        # learn.test_random(data_set, nb_test=1)
+        loss_mean, kl_div_mean, recon_loss_mean = learn.train(epoch)
+        # learn.test_random(data_set, nb_test=1)
+        loss_mean_test, kl_div_mean_test, recon_loss_mean_test = learn.test(epoch)
         # learn.save('/slow-1/ninon/output/models/weights/', 'slow-1/ninon/output/models/entire_model/', epoch)
         learn.save('./models/weights/', './models/entire_model/', epoch)
-        learn.piano_roll_recon('./models/entire_model/' + '_epoch_' + str(epoch) + '.pth')
-    # writer.close()
-    # learn.plot()
+        # learn.piano_roll_recon('./models/entire_model/' + '_epoch_' + str(epoch) + '.pth') TODO: solve the problem --'
 
     print('\nTraining Time in minutes =', (time() - time0) / 60)
