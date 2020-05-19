93d1e0b0b95a6a843f46635ede8e0871577679c0
Ninon Devis <ninon.devis@gmail.com>
2020-05-19 23:19:36 +0200

    Rewrite resume training and trials on recon from latent

--- learn.py
+++ learn.py
@@ -7,6 +7,7 @@ import random
 from guppy import hpy
 import os
 import pretty_midi
+from music21 import *
 # VAE model
 from vae import VaeModel
 from vae import HierarchicalDecoder, HierarchicalEncoder
@@ -26,9 +27,6 @@ dec_hidden_size = 1024
 num_layers = 2
 num_subsequences = 8
 seq_length = 128
-
-# Tensorboard initialization
-writer = SummaryWriter('./output/runs')
 
 
 class Learn:
@@ -63,6 +61,7 @@ class Learn:
         self.recon_loss_mean_test = 0
 
     def train(self, epoch, log_interval=10):
+        writer = SummaryWriter('./output/runs')
         self.model.train()
         for batch_idx, x in tqdm(enumerate(self.train_loader), total=len(self.train_set) // self.batch_size):
             x = x.to(self.device)
@@ -81,20 +80,22 @@ class Learn:
             loss.backward()
             # Optimizes weights
             self.optimizer.step()
-            if batch_idx % log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    self.iter_train, batch_idx * len(x), len(self.train_loader.dataset),
-                    100. * batch_idx / len(self.train_loader), self.loss_mean))
+            # if batch_idx % log_interval == 0:
+            #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            #         self.iter_train, batch_idx * len(x), len(self.train_loader.dataset),
+            #         100. * batch_idx / len(self.train_loader), self.loss_mean))
             if self.iter_train > 10 and self.beta < 1:
                 self.beta += 0.0025
             self.iter_train += 1
-            return self.loss_mean, self.kl_div_mean, self.recon_loss_mean
         with torch.no_grad():
             writer.add_scalar('data/loss_mean', self.loss_mean / self.iter_train, epoch)
             writer.add_scalar('data/kl_div_mean', self.kl_div_mean / self.iter_train, epoch)
             writer.add_scalar('data/reconst_loss_mean', self.recon_loss_mean / self.iter_train, epoch)
+            writer.close()
+        return self.loss_mean, self.kl_div_mean, self.recon_loss_mean
 
     def test(self, epoch):
+        writer = SummaryWriter('./output/runs')
         self.model.eval()
         with torch.no_grad():
             for batch_idx, x in tqdm(enumerate(self.test_loader), total=len(self.test_set) // self.batch_size):
@@ -110,53 +111,84 @@ class Learn:
                 loss = recon_loss + self.beta * kl_div
                 self.loss_mean_test += loss.detach()
                 self.iter_test += 1
+        with torch.no_grad():
             writer.add_scalar('data/loss_mean_TEST', self.loss_mean_test / self.iter_test, epoch)
             writer.add_scalar('data/kl_div_mean_TEST', self.kl_div_mean_test / self.iter_test, epoch)
             writer.add_scalar('data/reconst_loss_mean_TEST', self.recon_loss_mean_test / self.iter_test, epoch)
         # Print stuffs
         print('\nTest set: Average Loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             self.loss_mean_test, loss, len(self.test_loader.dataset), 100. * loss / len(self.test_loader)))
-
-    # generate random index for testing random data
-    def test_random(self, data_set, nb_test):
-        rand_ind = [random.randint(0, len(data_set)) for i in range(nb_test)]
-        ind = 0
-
-        for i in range(nb_test):
-            print('-' * 50)
-            print(data_set[rand_ind[ind]])
-            print('-')
-            x = data_set[rand_ind[ind]].unsqueeze(0)
-            x = x.to(self.device)
-            mu, sigma, latent, x_recon = self.model(x)
-            print(x_recon.max(dim=2))
-            ind += 1
+        return self.loss_mean_test, self.kl_div_mean_test, self.recon_loss_mean_test
 
     def save(self, model_weights_saving_path, entire_model_saving_path, epoch):
         # Save entire model
         if not os.path.exists(entire_model_saving_path):
             os.makedirs(entire_model_saving_path)
-        torch.save({'epoch': epoch,
-                    'model_state_dict': self.model.state_dict(),
-                    'optimizer_state_dict': self.optimizer.state_dict(),
-                    'loss': self.loss_mean},
-                   entire_model_saving_path + '_epoch_' + str(epoch) + '.pth')
+        torch.save(self.model, entire_model_saving_path + '_epoch_' + str(epoch) + '.pth')
         # Save only the weights
         if not os.path.exists(model_weights_saving_path):
             os.makedirs(model_weights_saving_path)
         torch.save(self.model.state_dict(), model_weights_saving_path + '_epoch_' + str(epoch) + '.pth')
 
-    def resume_training(self, entire_model_saving_path):
+    def resume_training(self, entire_model_saving_path, epoch):  # Specify the wishing epoch resuming here
         model = self.model
-        optimizer = self.optimizer
-        checkpoint = torch.load(entire_model_saving_path)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict((checkpoint['optimizer_state_dict']))
-        self.epoch = checkpoint['epoch']
-        self.loss_mean = checkpoint['loss']
+        state_dict = torch.load(entire_model_saving_path + '_epoch_' + str(epoch) + '.pth')
         model.eval()
 
-    def piano_roll_recon(self, entire_model_saving_path, fs=100, program=0):
+    # generate random index for testing random data TODO: FIX THE PLOT
+    def test_random(self, data_set, nb_test, fs=100, program=0):
+        rand_ind = [random.randint(0, len(data_set)) for i in range(nb_test)]
+        ind = 0
+
+        for i in range(nb_test):
+            print('-' * 50)
+            print('initial random tensor', rand_ind, data_set[rand_ind[ind]].max(dim=1))
+            print('-')
+            x = data_set[rand_ind[ind]].unsqueeze(0)
+            x = x.to(self.device)
+            mu, sigma, latent, x_recon = self.model(x)
+            print(x_recon.max(dim=2))
+            print('shape unsqueeze', x_recon.shape)
+            x_recon_squeeze = x_recon.squeeze(0).detach()
+            print('shape x squeezed: ', x_recon_squeeze.shape)
+            notes, frames = x_recon_squeeze[0], x_recon_squeeze[1]
+            pm = pretty_midi.PrettyMIDI()
+            instrument = pretty_midi.Instrument(program=program)
+            # Pad 1 column of zeros to acknowledge initial and ending events
+            piano_roll = np.pad(x_recon_squeeze, [(0, 0), (1, 1)], 'constant')
+            # Use changes in velocities to find note on/note off events
+            velocity_changes = np.nonzero(np.diff(piano_roll).T)
+            # Keep track on velocities and note on times
+            prev_velocities = np.zeros(notes, dtype=int)
+            note_on_time = np.zeros(notes)
+            for time, note in zip(*velocity_changes):
+                # Use time + 1 because of padding above
+                velocity = piano_roll[notes-1, time + 1]
+                time = time / fs
+                if velocity > 0:
+                    if prev_velocities[note] == 0:
+                        note_on_time[note] = time
+                        prev_velocities[note] = velocity
+                else:
+                    pm_note = pretty_midi.Note(
+                        velocity=prev_velocities[note],
+                        pitch=note,
+                        start=note_on_time[note],
+                        end=time)
+                    instrument.notes.append(pm_note)
+                    prev_velocities[note] = 0
+                pm.instruments.append(instrument)
+                # midi_output_path = './midi_output_test/' + str(nb_test) + '.mid'
+                # if not os.path.exists(midi_output_path):
+                #     os.makedirs(midi_output_path)
+                pm.write('midi_output_test_' + str(nb_test) + '.mid')
+                mid_plot = converter.parse('midi_output_test_' + str(nb_test) + '.mid')
+                mid_plot.plot('pianoroll')
+                # return pm
+            # print(pm)
+            ind += 1
+
+    def piano_roll_recon(self, entire_model_saving_path, fs=100, program=0):  # TODO: FIX THE RECON FROM LATENT
         # state_dict = torch.load(entire_model_saving_path)
         # loss_mean, kl_div_mean, recon_loss_mean = learn.train()
         model = self.model
@@ -198,9 +230,8 @@ class Learn:
                 prev_velocities[note]=0
             pm.instruments.append(instrument)
             return pm
-        print(pm)
+        print('PianoRoll', pm)
 
-    # # TODO:Put the function inside the test and train function maybe that will work
     # def fill_tensorboard(self, epoch):
     #     print("Adding means to tensorboard")
     #     with torch.no_grad():
