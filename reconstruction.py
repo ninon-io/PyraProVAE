460e909d4d2dad82d200cb581e915aa1d7d26e90
Ninon Devis <ninon.devis@gmail.com>
2020-06-12 18:36:21 +0200

    Trials CPU

--- reconstruction.py
+++ reconstruction.py
@@ -31,7 +31,7 @@ def reconstruction(args, epoch):
             axi.set_title("Original number " + str(rand_ind[ind]))
         else:
             dataset[rand_ind[ind]][dataset[rand_ind[ind]] > 0] = 1
-            _, _, _, x_reconstruct = model(dataset[rand_ind[ind]].unsqueeze(0))
+            _, _, _, x_reconstruct = model(dataset[rand_ind[ind]].unsqueeze(0)).numpy()
             x_reconstruct = x_reconstruct.squeeze(0).squeeze(0).detach()
             axi.matshow(x_reconstruct, alpha=1)
             # write row/col indices as axes' title for identification
