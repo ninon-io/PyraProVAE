7c7faec66e4c3ea5cd07056cc4a5c936d9d3ecfa
Ninon Devis <ninon.devis@gmail.com>
2020-06-12 17:49:11 +0200

    Trials on reconstruction

--- reconstruction.py
+++ reconstruction.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import data_loader
 import random
 import torch
+import numpy as np
 import os
+import argparse
 
 
-def reconstruction(args, model, epoch):
+def reconstruction(args, epoch):
     dataset = data_loader.PianoRollRep(args.midi_path)
     # Load the entire model
     model = torch.load(args.model_path + '_epoch_' + str(epoch) + '.pth')
@@ -13,19 +15,17 @@ def reconstruction(args, model, epoch):
 
     # Plot settings
     nrows, ncols = 4, 2  # array of sub-plots
-    figsize = [8, 20]  # figure size, inches
+    figsize = np.array([8, 20])  # figure size, inches
     # create figure (fig), and array of axes (ax)
     fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
 
     # generate random index for testing random data
-    rand_ind = [random.randint(0, len(dataset)) for i in range(nrows)]
+    rand_ind = np.array([random.randint(0, len(dataset)) for i in range(nrows)])
     ind = 0
 
     for i, axi in enumerate(ax.flat):
-        # i runs from 0 to (nrows*ncols-1)
-        # axi is equivalent with ax[rowid][colid]
         if i % 2 == 0:
-            piano_roll = dataset[rand_ind[ind]]
+            piano_roll = np.array(dataset[rand_ind[ind]])
             axi.matshow(piano_roll, alpha=1)
             # write row/col indices as axes' title for identification
             axi.set_title("Original number " + str(rand_ind[ind]))
@@ -33,7 +33,7 @@ def reconstruction(args, model, epoch):
             dataset[rand_ind[ind]][dataset[rand_ind[ind]] > 0] = 1
             model.to(args.device)
             mu, sigma, latent, x_reconstruct = model(dataset[rand_ind[ind]].unsqueeze(0))
-            x_reconstruct = x_reconstruct.squeeze(0).squeeze(0).detach().numpy()
+            x_reconstruct = x_reconstruct.squeeze(0).squeeze(0).detach()
             axi.matshow(x_reconstruct, alpha=1)
             # write row/col indices as axes' title for identification
             axi.set_title("Reconstruction number " + str(rand_ind[ind]))
@@ -45,3 +45,16 @@ def reconstruction(args, model, epoch):
         os.makedirs(args.figure_reconstruction_path)
     plt.savefig(args.figure_reconstruction_path + 'epoch_' + str(epoch))
     # plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Reconstruction')
+    parser.add_argument('--device', type=str, default='cuda:0', help='device cuda or cpu')
+    parser.add_argument('--midi_path', type=str, default='/fast-1/mathieu/datasets/maestro_folders/train',
+                        help='path to midi folder')
+    parser.add_argument('--model_path', type=str, default='/slow-2/ninon/pyrapro/models/entire_model/',
+                        help='path to the saved model')
+    args = parser.parse_args()
+    epoch = 0
+    reconstruction(args, epoch)
+
