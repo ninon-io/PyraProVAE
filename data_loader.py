d77dab833c3c2ac6186cf6f3e3825a7e408ebbef
Ninon Devis <ninon.devis@gmail.com>
2020-06-17 23:14:57 +0200

    Trial

--- data_loader.py
+++ data_loader.py
@@ -46,7 +46,7 @@ class PianoRollRep(Dataset):
         return self.nb_bars
 
     def __getitem__(self, index):
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=2.342, std=12.476)])
+        transform = transforms.Compose([transforms.Normalize(mean=2.342, std=12.476)])
         sample = torch.load(self.bar_dir + '/' + self.bar_files[index])
         norm_sample = transform(sample)
         return norm_sample
