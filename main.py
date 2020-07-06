a3d4a45aac67723dccd88c7c937611f181a71352
Ninushkat <ninon.devis@ircam.fr>
2020-07-06 22:49:52 +0200

    lr=10e-4

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ parser.add_argument('--batch_size', type=int, default=64, help='input batch size
 parser.add_argument('--subsample', type=int, default=0, help='train on subset')
 parser.add_argument('--epochs', type=int, default=150, help='number of epochs to train')
 parser.add_argument('--nbworkers', type=int, default=3, help='')
-parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
 parser.add_argument('--seed', type=int, default=1, help='random seed')
 # parser.add_argument('--log-interval', type=int, default=10, help='how many batches to wait before logging training status')
 # parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
