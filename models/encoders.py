8bda60ffb61f59d31e55e860e57be423d28ada4f
Philippe Esling <esling@touriste-147-10.ircam.fr>
2020-07-15 18:28:44 +0200

    Working out dat tasty CNN

--- models/encoders.py
+++ models/encoders.py
@@ -91,7 +91,7 @@ class EncoderCNN(nn.Module):
         hidden_size = args.enc_hidden_size
         in_channel = 1 if len(args.input_size) < 3 else args.input_size[0] #in_size is (C,H,W) or (H,W)
         kernel = [3, 13]
-        stride = [1, 1]
+        stride = [2, 1]
         """ First do a CNN """
         for l in range(n_layers):
             dil = 1
@@ -311,7 +311,7 @@ class DecoderCNN(nn.Module):
         self.cnn_size = cnn_size
         size = args.cnn_size
         kernel = [3, 13]
-        stride = [1, 1]
+        stride = [2, 1]
         in_size = args.latent_size
         hidden_size = args.dec_hidden_size
         self.mlp = nn.Sequential()
