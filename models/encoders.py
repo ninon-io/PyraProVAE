3f9af967fceaec8ad3817a18112e68bd3be07a52
Philippe Esling <esling@touriste-147-10.ircam.fr>
2020-07-16 20:07:16 +0200

    Trying to correct hierarchical version

--- models/encoders.py
+++ models/encoders.py
@@ -311,8 +311,6 @@ class EncoderHierarchical(nn.Module):
     def forward(self, x, ctx=None):
         self.RNN.flatten_parameters()
         x, _ = self.RNN(x)
-        print(x.shape)
-        x = x[-1]
         x = x.transpose_(0, 1).contiguous()
         x = x.view(x.size(0), -1)
         x = torch.tanh(self.bn_enc(self.linear_enc(x)))
