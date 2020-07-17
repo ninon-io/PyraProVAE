6d74a343fa3ef7692d38da239af45d6e94fbb48e
Ninon Devis <ninon.devis@gmail.com>
2020-07-17 18:20:10 +0200

    run_tests_models

--- run_tests_models.py
+++ run_tests_models.py
@@ -5,15 +5,6 @@ parser = argparse.ArgumentParser()
 #
 parser.add_argument('--midi_path', type=str, default='/fast-1/mathieu/datasets/', help='path to midi folder')
 parser.add_argument("--dataset", type=str, default="nottingham", help="maestro | nottingham | bach_chorales | midi_folder")
-# Novel arguments to try for winning model
-parser.add_argument('--frame_bar',      type=int, default=64,       help='put a power of 2 here')
-parser.add_argument('--score_type',     type=str, default='mono',   help='use mono measures or poly ones')
-parser.add_argument('--score_sig',      type=str, default='4_4',    help='rhythmic signature to use (use "all" to bypass)')
-parser.add_argument('--data_normalize', type=int, default=1,        help='normalize the data')
-parser.add_argument('--data_binarize',  type=int, default=1,        help='binarize the data')
-parser.add_argument('--data_pitch',     type=int, default=1,        help='constrain pitches in the data')
-parser.add_argument('--data_export',    type=int, default=0,        help='recompute the dataset (for debug purposes)')
-parser.add_argument('--data_augment',   type=int, default=1,        help='use data augmentation')
 # Model Saving and reconstruction
 parser.add_argument('--output_path', type=str, default='output/', help='major path for data output')
 # Model Parameters
@@ -22,24 +13,10 @@ parser.add_argument("--beta", type=float, default=1., help='value of beta regula
 parser.add_argument("--beta_delay", type=int, default=0, help='delay before using beta')
 parser.add_argument("--encoder_type", type=str, default="gru", help='mlp | cnn | res-cnn | gru | cnn-gru | hierarchical')
 # PyraPro and vae_mathieu specific parameters: dimensions of the architecture
-parser.add_argument('--enc_hidden_size', type=int, default=512, help='do not touch if you do not know')
 parser.add_argument('--latent_size', type=int, default=128, help='do not touch if you do not know')
-parser.add_argument('--cond_hidden_size', type=int, default=1024, help='do not touch if you do not know')
-parser.add_argument('--cond_output_dim', type=int, default=512, help='do not touch if you do not know')
-parser.add_argument('--dec_hidden_size', type=int, default=512, help='do not touch if you do not know')
-parser.add_argument('--num_layers', type=int, default=2, help='do not touch if you do not know')
-parser.add_argument('--num_subsequences', type=int, default=8, help='do not touch if you do not know')
-parser.add_argument('--num_classes', type=int, default=2, help='number of velocity classes')
-parser.add_argument('--initialize', type=int, default=0, help='use initialization on the model')
 # Optimization parameters
-parser.add_argument('--batch_size', type=int, default=64, help='input batch size')
-parser.add_argument('--subsample', type=int, default=0, help='train on subset')
 parser.add_argument('--epochs', type=int, default=300, help='number of epochs to train')
-parser.add_argument('--nbworkers', type=int, default=3, help='')
-parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
-parser.add_argument('--seed', type=int, default=1, help='random seed')
-# Running
-parser.add_argument('--n_runs',             default=10,             type=int,       help='')
+parser.add_argument('--n_runs',             default=15,             type=int,       help='')
 # Parse the arguments
 args = parser.parse_args()
 # Dataset argument
@@ -52,9 +29,9 @@ encoder_type = ['mlp', 'cnn', 'res_cnn', 'gru', 'cnn_gru', 'hierarchical']
 latent_size = [256, 128, 64, 32, 16, 4]
 
 # Using list comprehension to compute all possible permutations
-res = [[i, j, k] for i in [args.model]
+res = [[i, j, k] for i in [model]
                  for j in [encoder_type]
-                 for k in [args.latent_size]]
+                 for k in [latent_size]]
 
 
 configurations = {'test': 0, 'full': 1, 'large': 2}
@@ -72,6 +49,7 @@ with open(run_name, 'w') as file:
             cmd_str += ' --encoder_type ' + vals[1]
             cmd_str += ' --latent_size ' + vals[2]
             cmd_str += ' --epochs ' + str(args.epochs)
+            cmd_str += ' --k_run ' + str(r)
             print(cmd_str)
             file.write(cmd_str + '\n')
 os.system('chmod +x ' + run_name)
