d8178b4f6ce86494ffdac8f38472a197aef3fd43
Ninushkat <ninon.devis@ircam.fr>
2020-07-28 16:27:13 +0200

    GRU CNN Decoder

--- models/encoders.py
+++ models/encoders.py
@@ -672,6 +672,7 @@ class DecoderCNNGRU(nn.Module):
             if i != 0:
                 out = out.squeeze(1)
             out = torch.stack(x, 1)
+            print('after loop out:', out.shape)
         out = out.unsqueeze(1).view(-1, 1, self.cnn_size[0], self.cnn_size[1])  # TODO
         for m in range(len(self.net)):
             out = self.net[m](out)
